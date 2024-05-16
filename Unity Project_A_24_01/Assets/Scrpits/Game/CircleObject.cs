e3e9f2d941706e37085f23c9698bea948a8682bc
kyo1109kr <kyo011109@naver.com>
2024-05-16 15:46:34 +0900

    [Feet]수박과일 게임

--- Unity Project_A_24_01/Assets/Scrpits/Game/CircleObject.cs
+++ Unity Project_A_24_01/Assets/Scrpits/Game/CircleObject.cs
@@ -7,12 +7,21 @@ public class CircleObject : MonoBehaviour
     public bool isDrag;            //마우스 drag 판단
     public bool isUsed;            //사용 완료 체크
     Rigidbody2D rigidbody2D;       //2D 강체 선언
-    // Start is called before the first frame update
+    
+    public int index;              //과일 번호 설정
+                                   // Start is called before the first frame update
+
+    void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();    //오브젝트의 강체에 접근
+        isUsed = false;                              //시작할때 사용이 안되었다고 입력 
+        rigidbody2D.simulated = false;                //물리 행동이 처음에는 동작하지 않게 설정 
+    }
     void Start()
     {
-        isUsed = false;                               //시작할때 사용이 안되었다고 입력
-        rigidbody2D = GetComponent<Rigidbody2D>();    //오브젝트의 강체에 접근
-        rigidbody2D.simulated = false;                //물리 행동이 처음에는 동작하지 않게 설정
+        
+        
+        
     }
 
     // Update is called once per frame
@@ -53,10 +62,44 @@ public class CircleObject : MonoBehaviour
         isUsed = true;                   //사용 완료 되었다  true
         rigidbody2D.simulated = true;    //뮬리 시뮬레이션 사용함  true
 
-        GameObject temp = GameObject.FindWithTag("GameManager");           //Scene에서 GameManager Tag 가지고 있는 오브젝트를 가져온다
+        GameObject temp = GameObject.FindGameObjectWithTag("GameManager");           //Scene에서 GameManager Tag 가지고 있는 오브젝트를 가져온다
         if (temp != null)                                                 //해당 오브젝트가 있을 경우
         {
             temp.gameObject.GetComponent<GameManager>().GenObject();      //GameManager의 GemObject 함수를 호출
+        }
+    }
+
+    public void Used()
+    {
+        isDrag = false;                  //드래그 중이다  false
+        isUsed = true;                   //사용 완료 되었다  true
+        rigidbody2D.simulated = true;    //뮬리 시뮬레이션 사용함  true
+    }
+    public void OnCollisionEnter2D(Collision2D collision)                        //해당 오브젝트가 충돌 했을 때 OnCollisionEnter2D
+    {
+        if (index >= 7)
+            return;
+
+        if(collision.gameObject.tag == "Fruit")                                     //충돌이 같은 과일일 경우
+        {
+            CircleObject temp = collision.gameObject.GetComponent<CircleObject>();   //충돌한 물체에서 같은 Class를 받아온다
+
+            if(temp.index == index)                                                //충돌 indwx와 내 index가 같다
+            {
+
+                if(gameObject.GetInstanceID() > collision.gameObject.GetInstanceID()) //2개 합쳐서 1개를 만들기 위해서 ID 검사 후 큰것만
+                {
+                    //GameManager에서 합친 오브젝트를 생성
+                    GameObject tempGameManager = GameObject.FindWithTag("GameManager");       //Scene에서 GameManager Tag 가지고 있는 오브젝트를 가져온다
+                    if (tempGameManager != null)                                                     //해당 오브젝트 가 있을 경우
+                    {
+                        tempGameManager.gameObject.GetComponent<GameManager>().MergeObject(index, gameObject.transform.position);
+                    }
+                    Destroy(temp.gameObject);                                               //충돌한 물체 제거
+                    Destroy(gameObject);                                                    //자신도 제거
+                }
+
+            }
         }
     }
 }
--- Unity Project_A_24_01/Assets/Scrpits/Game/GameManager.cs
+++ Unity Project_A_24_01/Assets/Scrpits/Game/GameManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject CircleObject;      //물체 프라팹을 가져온다.
+    public GameObject[] circleObject;      //물체 프라팹을 가져온다.  (배열로 변경)
     public Transform genTransform;       //생성 위치 설정
     public float timCheck;               //생성 시간 설정 변수(float)
     public bool isGen;                   //생성 체크 (bool)
@@ -29,10 +29,19 @@ public class GameManager : MonoBehaviour
             timCheck -= Time.deltaTime;                                  //매 프레임 돌아가면서 시간을 감소 시킨다
             if(timCheck <= 0.0f)                                         //0초 이하가 되렀을 경우
             {
-                GameObject Temp = Instantiate(CircleObject);           //프리팹 생성후 Temp오브젝트
+                int RandNumber = Random.Range(0, 3);                     //0~2의 랜덤 넘버 생성
+                GameObject Temp = Instantiate(circleObject[RandNumber]);           //프리팹 생성후 Temp오브젝트
                 Temp.transform.position = genTransform.position;         //고정 위치에 생성 시킨다
                 isGen = true;
             }
         }
     }
+
+    public void MergeObject(int index, Vector3 position)                   //충돌한 물체의 index 번호와 위치를 가져온다
+    {
+        GameObject Temp = Instantiate(circleObject[index]);        //생성된 과일 오브젝트를 Temp에 넣는다
+        Temp.transform.position = position;                         //Temp 오브젝트의 위치는 함수로 받아온 위치값
+        Temp.GetComponent<CircleObject>().Used();                 //생성되어있을때 사용되었다고 표시 해줘야함
+    }
+
 }
